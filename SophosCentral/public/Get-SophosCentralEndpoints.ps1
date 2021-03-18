3d6594704c1b6a9092e7f211ed293e8922d50bb2
Simon Watson <simon.r.watson@outlook.com>
2021-03-18 12:51:54 +0800

    Rename Get-SophosCentralEndpoints

--- SophosCentral/public/Get-SophosCentralEndpoints.ps1
+++ SophosCentral/public/Get-SophosCentralEndpoints.ps1
@@ -1,4 +1,4 @@
-function Get-SophosCentralEndpoint {
+function Get-SophosCentralEndpoints {
     $uriChild = '/endpoint/v1/endpoints'
     $uri = [System.Uri]::New($GLOBAL:SophosCentral.RegionEndpoint + $uriChild)
     Invoke-SophosCentralWebRequest -Uri $uri
