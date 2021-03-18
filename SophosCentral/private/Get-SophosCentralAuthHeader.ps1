75a6b13c18585908a82ed4d46c5953006c746c4a
Simon Watson <simon.r.watson@outlook.com>
2021-03-18 12:52:17 +0800

    Update Get-SophosCentralAuthHeader.ps1

--- SophosCentral/private/Get-SophosCentralAuthHeader.ps1
+++ SophosCentral/private/Get-SophosCentralAuthHeader.ps1
@@ -1,15 +1,26 @@
 function Get-SophosCentralAuthHeader {
     [CmdletBinding()]
     param (
-        [switch]$Initial
+        [switch]$Initial,
+        [switch]$PartnerInitial
     )
 
     if ($GLOBAL:SophosCentral) {
         $header = @{
-            Authorization = 'Bearer ' + $GLOBAL:SophosCentral.access_token
+            Authorization = 'Bearer ' + (Unprotect-Secret -Secret $GLOBAL:SophosCentral.access_token)
         }
-        if ($Initial -ne $true) {
-            $header.Add('X-Tenant-ID', $GLOBAL:SophosCentral.TenantID)
+        if (($Initial -ne $true) -and ($PartnerInitial -ne $true)) {
+            switch ($GLOBAL:SophosCentral.IDType) {
+                'partner' {
+                    $header.Add('X-Tenant-ID', $GLOBAL:SophosCentral.CustomerTenantID)
+                }
+                'tenant' {
+                    $header.Add('X-Tenant-ID', $GLOBAL:SophosCentral.TenantID)
+                }
+            }
+        }
+        if ($PartnerInitial) {
+            $header.Add('X-Partner-ID', $GLOBAL:SophosCentral.TenantID)
         }
         $header
     }
