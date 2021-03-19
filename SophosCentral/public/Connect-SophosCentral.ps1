c3a6a4ed397eb472a3a1efda94d07f7a6fbab8a5
Simon Watson <simon.r.watson@outlook.com>
2021-03-19 14:06:28 +0800

    Update Connect-SophosCentral.ps1

--- SophosCentral/public/Connect-SophosCentral.ps1
+++ SophosCentral/public/Connect-SophosCentral.ps1
@@ -14,6 +14,8 @@ function Connect-SophosCentral {
         The client ID from the Sophos Central API credential/service principal
     .PARAMETER ClientSecret
         The client secret from the Sophos Central API credential/service principal
+    .PARAMETER AccessTokenOnly
+        Internal use (for this module) only. Used to generate a new access token, when the current one expires
     .EXAMPLE
         Connect-SophosCentral -ClientID "asdkjsdfksjdf" -ClientSecret (Read-Host -AsSecureString -Prompt "Client Secret:")
     #>
@@ -21,7 +23,8 @@ function Connect-SophosCentral {
     param (
         [Parameter(Mandatory = $true)]
         [String]$ClientID,
-        [SecureString]$ClientSecret
+        [SecureString]$ClientSecret,
+        [Switch]$AccessTokenOnly
     )
 
     if ($null -eq $ClientSecret) {
@@ -36,18 +39,34 @@ function Connect-SophosCentral {
         client_secret = Unprotect-Secret -Secret $ClientSecret
         scope         = 'token'
     }
-    $response = Invoke-WebRequest -Uri $loginUri -Body $body -ContentType 'application/x-www-form-urlencoded' -Method Post -UseBasicParsing
+    try {
+        $response = Invoke-WebRequest -Uri $loginUri -Body $body -ContentType 'application/x-www-form-urlencoded' -Method Post -UseBasicParsing
+    }
+    catch {
+        throw "Error requesting access token: $($_)"
+    }
+    
     if ($response.Content) {
         $authDetails = $response.Content | ConvertFrom-Json
         $expiresAt = (Get-Date).AddSeconds($authDetails.expires_in - 60)
-        $authDetails | Add-Member -MemberType NoteProperty -Name expires_at -Value $expiresAt
-        $authDetails.access_token = $authDetails.access_token | ConvertTo-SecureString -AsPlainText -Force
-        $GLOBAL:SophosCentral = $authDetails
 
-        $tenantInfo = Get-SophosCentralTenantInfo
-        $GLOBAL:SophosCentral | Add-Member -MemberType NoteProperty -Name GlobalEndpoint -Value $tenantInfo.apiHosts.global
-        $GLOBAL:SophosCentral | Add-Member -MemberType NoteProperty -Name RegionEndpoint -Value $tenantInfo.apiHosts.dataRegion
-        $GLOBAL:SophosCentral | Add-Member -MemberType NoteProperty -Name TenantID -Value $tenantInfo.id
-        $GLOBAL:SophosCentral | Add-Member -MemberType NoteProperty -Name IDType -Value $tenantInfo.idType
+        if ($AccessTokenOnly -eq $true) {
+            $GLOBAL:SophosCentral.access_token = $authDetails.access_token | ConvertTo-SecureString -AsPlainText -Force
+            $GLOBAL:SophosCentral.expires_at = $expiresAt
+        }
+        else {
+            $authDetails | Add-Member -MemberType NoteProperty -Name expires_at -Value $expiresAt
+            $authDetails.access_token = $authDetails.access_token | ConvertTo-SecureString -AsPlainText -Force
+            $GLOBAL:SophosCentral = $authDetails
+
+            $tenantInfo = Get-SophosCentralTenantInfo
+            $GLOBAL:SophosCentral | Add-Member -MemberType NoteProperty -Name GlobalEndpoint -Value $tenantInfo.apiHosts.global
+            $GLOBAL:SophosCentral | Add-Member -MemberType NoteProperty -Name RegionEndpoint -Value $tenantInfo.apiHosts.dataRegion
+            $GLOBAL:SophosCentral | Add-Member -MemberType NoteProperty -Name TenantID -Value $tenantInfo.id
+            $GLOBAL:SophosCentral | Add-Member -MemberType NoteProperty -Name IDType -Value $tenantInfo.idType
+
+            $GLOBAL:SophosCentral | Add-Member -MemberType NoteProperty -Name client_id -Value $ClientID
+            $GLOBAL:SophosCentral | Add-Member -MemberType NoteProperty -Name client_secret -Value $ClientSecret
+        }
     }
 }
