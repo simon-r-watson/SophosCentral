6febd493bc73600a845a7dc9687045eca0bd23b4
Simon Watson <simon.r.watson@outlook.com>
2021-03-19 14:06:16 +0800

    Create Test-SophosCentralAuth.ps1

---
+++ SophosCentral/private/Test-SophosCentralAuth.ps1
@@ -0,0 +1,25 @@
+function Test-SophosCentralAuth {
+    if ($GLOBAL:SophosCentral) {
+        $date = Get-Date
+        if ($GLOBAL:SophosCentral.expires_at -le $date) {
+            Write-Verbose "Access token has expired"
+            #request new token
+            try {
+                Write-Verbose "Attempting to obtain new access token"
+                Connect-SophosCentral -ClientID $GLOBAL:SophosCentral.client_id -ClientSecret -ClientID $GLOBAL:SophosCentral.client_secret -AccessTokenOnly
+                Write-Verbose "Testing new access token"
+                Test-SophosCentralAuth
+            }
+            catch {
+                throw "error requesting new token"
+            }           
+        }
+        else {
+            return $true
+        }
+    }
+    else {
+        throw "You must connect with 'Connect-SophosCentral' before running any commands"
+        return $false
+    }
+}
