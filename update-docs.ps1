85aa7a559d2481f58a84de926bd4e201d116ff9c
Simon Watson <simon.r.watson@outlook.com>
2021-04-15 14:57:59 +0800

    Update update-docs.ps1

--- update-docs.ps1
+++ update-docs.ps1
@@ -5,8 +5,9 @@ if (Get-Module SophosCentral) {
 Install-Module platyps -Scope CurrentUser -Force
 Import-Module .\SophosCentral\SophosCentral.psd1
 New-MarkdownHelp -Module SophosCentral -OutputFolder .\docs -Force
-New-ExternalHelp -Path ".\docs" -OutputPath output\SophosCentral\en-us\ -Force
+New-ExternalHelp -Path .\docs -OutputPath output\SophosCentral\en-us\ -Force
 New-ExternalHelp -Path .\docs -OutputPath output\SophosCentral\en-au\ -Force
+New-ExternalHelp -Path .\docs -OutputPath output\SophosCentral\en-gb\ -Force
 
 Update-MarkdownHelp .\docs -Force
 
