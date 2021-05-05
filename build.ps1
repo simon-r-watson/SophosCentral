85e63f6af104327119223c81be65c3ee45d7d4a5
Simon Watson <simon.r.watson@outlook.com>
2021-05-05 17:51:35 +0800

    Update build.ps1

--- build.ps1
+++ build.ps1
@@ -6,26 +6,24 @@ Derived from scripts written by Warren F. (RamblingCookieMonster)
 
 [cmdletbinding()]
 param ($Task = 'Default')
-Write-Output "Starting build"
+Write-Output 'Starting build'
 
 # Grab nuget bits, install modules, set build variables, start build.
-Write-Output "  Install Dependent Modules"
+Write-Output '  Install Dependent Modules'
 Get-PackageProvider -Name NuGet -ForceBootstrap | Out-Null
-Install-Module InvokeBuild, PSDeploy, BuildHelpers, PSScriptAnalyzer, Coveralls -force -Scope CurrentUser
+Install-Module InvokeBuild, PSDeploy, BuildHelpers, PSScriptAnalyzer, Coveralls -Force -Scope CurrentUser
 Install-Module Pester -Force -SkipPublisherCheck -Scope CurrentUser
 
-Write-Output "  Import Dependent Modules"
+Write-Output '  Import Dependent Modules'
 Import-Module InvokeBuild, BuildHelpers, PSScriptAnalyzer
 
 Set-BuildEnvironment
 
-Write-Output "  InvokeBuild"
+Write-Output '  InvokeBuild'
 Invoke-Build $Task -Result result
-if ($Result.Error)
-{
+if ($Result.Error) {
     exit 1
-}
-else 
-{
+} else {
+    Compress-Archive -Path .\output\SophosCentral -DestinationPath .\output\SophosCentral.zip
     exit 0
 }
