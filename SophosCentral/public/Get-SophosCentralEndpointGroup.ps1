e0809ff7502f039eaae1cd0efc615132f3b46e56
Simon Watson <sw@simon-watson.com>
2022-09-22 20:57:25 +0800

    initial endpoint groups

---
+++ SophosCentral/public/Get-SophosCentralEndpointGroup.ps1
@@ -0,0 +1,18 @@
+function Get-SophosCentralEndpointGroup {
+    <#
+    .SYNOPSIS
+        Get Endpoint groups in the directory
+    .DESCRIPTION
+        Get Endpoint groups in the directory
+    .EXAMPLE
+        Get-SophosCentralEndpointGroup
+    .LINK
+        https://developer.sophos.com/docs/endpoint-v1/1/routes/endpoint-groups/get
+    #>
+    [CmdletBinding()]
+    param (
+    )
+
+    $uri = [System.Uri]::New($SCRIPT:SophosCentral.RegionEndpoint + '/endpoint/v1/endpoint-groups')
+    Invoke-SophosCentralWebRequest -Uri $uri
+}
