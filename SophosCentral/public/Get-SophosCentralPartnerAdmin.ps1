1d25a072fcd454dba2e49b94c1102015bb1e8f6a
Simon Watson <sw@simon-watson.com>
2022-09-22 20:57:24 +0800

    Update Get-SophosCentralPartnerAdmin.ps1

--- SophosCentral/public/Get-SophosCentralPartnerAdmin.ps1
+++ SophosCentral/public/Get-SophosCentralPartnerAdmin.ps1
@@ -12,6 +12,12 @@ function Get-SophosCentralPartnerAdmin {
     [CmdletBinding()]
     param (
     )
+    
+    if ($SCRIPT:SophosCentral.IDType -ne 'partner') {
+        throw 'You are not currently logged in using a Sophos Central Partner Service Principal'
+    } else {
+        Write-Verbose 'currently logged in using a Sophos Central Partner Service Principal'
+    }
 
     $header = Get-SophosCentralAuthHeader -PartnerInitial
     $uri = [System.Uri]::New('https://api.central.sophos.com/partner/v1/admins')
