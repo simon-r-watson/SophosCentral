0452c44536fd9b75e44855ea26a2fecd3e4c5fcc
Simon Watson <sw@simon-watson.com>
2022-10-31 19:15:04 +0800

    Add filtering to endpoint migration example

--- Examples/EndpointMigration.ps1
+++ Examples/EndpointMigration.ps1
@@ -7,8 +7,11 @@ $SourceTenantID = '5af77067-4c7b-4ce9-93cc-39c9a2f54e4d'
 $DestinationTenantID = 'b2aaa273-2f27-4036-9e07-f1b1832d589c'
 
 #Get all endpoints in the source tenant
+#You must filter this if you only what to migrate a subset on the endpoints in the source tenant
 Connect-SophosCentralCustomerTenant -CustomerTenantID $SourceTenantID
 $Endpoints = Get-SophosCentralEndpoint
+#Filter example using Out-GridView
+$Endpoints = Get-SophosCentralEndpoint | Out-GridView -PassThru
 
 #Create the receive job in the destination tenant
 $jobDetails = Invoke-SophosCentralEndpointMigrationReceive -EndpointID ($Endpoints).id -SourceTenantID $SourceTenantID -DestinationTenantID $DestinationTenantID
