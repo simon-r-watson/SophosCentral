b4e5f817381b5e10344ecbb40f705b5dbe4b80be
Simon Watson <sw@simon-watson.com>
2022-10-31 15:29:21 +0800

    Add account health check - untested
    https://developer.sophos.com/account-health-check

---
+++ SophosCentral/public/Get-SophosCentralAccountHealthCheck.ps1
@@ -0,0 +1,18 @@
+function Get-SophosCentralAccountHealthCheck {
+    <#
+    .SYNOPSIS
+        The Account Health Check API allows you to retrieve a health report for your Sophos Central account indicating whether you are making the best use of your Sophos security products.
+    .DESCRIPTION
+        The Account Health Check API allows you to retrieve a health report for your Sophos Central account indicating whether you are making the best use of your Sophos security products.
+    .EXAMPLE
+        Get-SophosCentralAccountHealthCheck
+    .LINK
+        https://developer.sophos.com/account-health-check
+    #>
+    [CmdletBinding()]
+    param (
+    )
+
+    $uri = [System.Uri]::New($SCRIPT:SophosCentral.RegionEndpoint + '/account-health-check/v1/health-check')
+    Invoke-SophosCentralWebRequest -Uri $uri
+}
