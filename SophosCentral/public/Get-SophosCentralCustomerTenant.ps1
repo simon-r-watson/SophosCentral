37c066faf5c08db1ea0c8f99e153ba02eeff4b82
Simon Watson <sw@simon-watson.com>
2022-11-01 11:02:48 +0800

    fix issue testing service principal type #3

--- SophosCentral/public/Get-SophosCentralCustomerTenant.ps1
+++ SophosCentral/public/Get-SophosCentralCustomerTenant.ps1
@@ -18,7 +18,7 @@ function Get-SophosCentralCustomerTenant {
     param (
     )
     
-    if (((Test-SophosPartner) -and (Test-SophosEnterprise)) -eq $false) {
+    if (((Test-SophosPartner) -or (Test-SophosEnterprise)) -eq $false) {
         throw 'You are not currently logged in using a Sophos Central Partner/Enterprise Service Principal'
     }
 
