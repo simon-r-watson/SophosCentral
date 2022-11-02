e76fdbb27b69b6d965547152818e4e20ba2f7a8e
Simon Watson <sw@simon-watson.com>
2022-11-02 22:54:06 +0800

    Create Get-SophosCentralAdmin.ps1

---
+++ SophosCentral/public/Get-SophosCentralAdmin.ps1
@@ -0,0 +1,19 @@
+function Get-SophosCentralAdmin {
+    <#
+    .SYNOPSIS
+        Get admins listed in Sophos Central
+    .DESCRIPTION
+        Get admins listed in Sophos Central
+    .EXAMPLE
+        Get-SophosCentralAdmin
+    .LINK
+        https://developer.sophos.com/docs/common-v1/1/routes/admins/get
+    #>
+    [CmdletBinding()]
+    param (
+    )
+
+    $uriChild = '/common/v1/admins'
+    $uri = [System.Uri]::New($SCRIPT:SophosCentral.RegionEndpoint + $uriChild)
+    Invoke-SophosCentralWebRequest -Uri $uri
+}
