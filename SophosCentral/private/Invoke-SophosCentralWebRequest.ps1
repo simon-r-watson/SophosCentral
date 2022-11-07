9db69894a811c92fbd56f50a2a5a546fef763dd7
Simon Watson <sw@simon-watson.com>
2022-11-07 22:37:35 +0800

    Rewrite and consolidate
    Add support for get requests with a body, this should auto change it to use query params in the uri
    
    Consolidate requests types into one, instead of separate for get, post/put, delete

--- SophosCentral/private/Invoke-SophosCentralWebRequest.ps1
+++ SophosCentral/private/Invoke-SophosCentralWebRequest.ps1
@@ -25,91 +25,63 @@ function Invoke-SophosCentralWebRequest {
             throw $_
         }
     }
-    $finished = $false
 
-    #Get request
-    if ($method -eq 'Get') {
-        #query api and return the first page
-        $response = Invoke-RestMethod -Uri $uri -Headers $header -UseBasicParsing
-        if ($null -ne $response.items) {
-            $response.items
-        } else {
-            $response
-        }
-        
-        if ($response.pages.total -gt 1) {
-            #enterprise/partner tenant pagination - based on total returned from the initial lookup
-            #see here for details on pagination https://developer.sophos.com/getting-started > step 4
-            #doco says the initial page is 1, so the following pages start at 2 onwards
-            for ($i = 2; $i -le $response.pages.total; $i++) {
-                $nextUri = $uri.AbsoluteUri.Replace('pageTotal=true', "page=$($i.ToString())")
-                $responseLoop = Invoke-RestMethod -Uri $nextUri -Headers $header -UseBasicParsing
-                if ($null -ne $response.items) {
-                    $responseLoop.items
-                } else {
-                    $responseLoop
-                }
-            }
-        } else {
-            #standard pagination - based on nextKey value returned from the previous lookup
-            do {
-                if ($response.pages.nextKey) {
-                    if ($uri.AbsoluteUri -like '*``?*') {
-                        $nextUri = $uri.AbsoluteUri + '&pageFromKey=' + $response.pages.nextKey
-                    } else {
-                        $nextUri = $uri.AbsoluteUri + '?pageFromKey=' + $response.pages.nextKey
-                    }
-                    $response = Invoke-RestMethod -Uri $nextUri -Headers $header -UseBasicParsing
-                    $response.items
-                } else {
-                    $finished = $true
-                }
-            } while ($finished -eq $false)
-        }
-        
+
+    #inital request
+    $webRequest = @{
+        Uri             = $uri
+        Headers         = $header
+        UseBasicParsing = $true
+        Method          = $Method
     }
-
-    #Post/Put request
-    if ($method -in ('Post', 'Put')) {
+    if (($null -eq $body) -and ($Method -in ('Post', 'Put'))) {
         #API endpoints that use a 'post'/'put' require a body. If no body is present it will give an error back, so supply an empty body
-        if ($null -eq $body) {
-            $body = @{}
-        }
-        $bodyJson = $body | ConvertTo-Json
-        
-        #query api and return the first page
-        $response = Invoke-RestMethod -Uri $uri -Headers $header -Method $method -Body $bodyJson -ContentType 'application/json' -UseBasicParsing 
-        if ($null -ne $response.items) {
-            $response.items
-        } else {
-            $response
-        }
-        #loop through additional pages of results (if applicable)
-        do {
-            if ($response.pages.nextKey) {
-                $nextUri = $uri.AbsoluteUri + '?pageFromKey=' + $response.pages.nextKey
-                $response = Invoke-RestMethod -Uri $nextUri -Headers $header -Method $method -Body $bodyJson -ContentType 'application/json' -UseBasicParsing 
-                $response.items
-            } else {
-                $finished = $true
-            }
-        } while ($finished -eq $false)
+        $bodyTmp = @{} | ConvertTo-Json
+        $webRequest.Add('Body', $bodyTmp)
+    } elseif (($null -ne $Body) -and ($Method -eq 'Get')) {
+        $webRequest.Add('Body', $Body)
+    } elseif ($null -ne $Body) {
+        $webRequest.Add('Body', ($Body | ConvertTo-Json))
     }
 
-    #Delete request
-    if ($method -eq 'Delete') {        
-        #query api and return the first page
-        $response = Invoke-RestMethod -Uri $uri -Headers $header -Method Delete -ContentType 'application/json' -UseBasicParsing 
-        if ($null -ne $response.items) {
-            $response.items
-        } else {
-            $response
+    if ($Method -notin ('Delete', 'Get')) {
+        $Body.Add('ContentType', 'application/json')
+    }
+
+    #query api and return the first page
+    $response = Invoke-RestMethod @webRequest
+    if ($null -ne $response.items) {
+        $response.items
+    } else {
+        $response
+    }
+    
+    #pagination
+    $finished = $false
+    if ($response.pages.total -gt 1) {
+        #enterprise/partner tenant pagination - based on total returned from the initial lookup
+        #see here for details on pagination https://developer.sophos.com/getting-started > step 4
+        #doco says the initial page is 1, so the following pages start at 2 onwards
+        for ($i = 2; $i -le $response.pages.total; $i++) {
+            $webRequest['Uri'] = $uri.AbsoluteUri.Replace('pageTotal=true', "page=$($i.ToString())")
+            
+            $responseLoop = Invoke-RestMethod @webRequest
+            if ($null -ne $response.items) {
+                $responseLoop.items
+            } else {
+                $responseLoop
+            }
         }
-        #loop through additional pages of results (if applicable)
+    } else {
+        #standard pagination - based on nextKey value returned from the previous lookup
         do {
             if ($response.pages.nextKey) {
-                $nextUri = $uri.AbsoluteUri + '?pageFromKey=' + $response.pages.nextKey
-                $response = Invoke-RestMethod -Uri $nextUri -Headers $header -Method Delete -ContentType 'application/json' -UseBasicParsing 
+                if ($uri.AbsoluteUri -like '*``?*') {
+                    $webRequest['Uri'] = $uri.AbsoluteUri + '&pageFromKey=' + $response.pages.nextKey
+                } else {
+                    $webRequest['Uri'] = $uri.AbsoluteUri + '?pageFromKey=' + $response.pages.nextKey
+                }
+                $response = Invoke-RestMethod @webRequest
                 $response.items
             } else {
                 $finished = $true
