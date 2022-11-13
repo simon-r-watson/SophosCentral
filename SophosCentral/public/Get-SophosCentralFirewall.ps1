a9a7fcf09b1477e3a99fc1d05c6916b9564a8ce8
Simon Watson <sw@simon-watson.com>
2022-11-13 22:02:29 +0800

    add parameters to Get-SophosCentralFirewall

--- SophosCentral/public/Get-SophosCentralFirewall.ps1
+++ SophosCentral/public/Get-SophosCentralFirewall.ps1
@@ -6,14 +6,36 @@ function Get-SophosCentralFirewall {
         Get firewalls in Sophos Central
     .EXAMPLE
         Get-SophosCentralFirewall
+    .PARAMETER GroupId
+        Firewall group ID, or 'ungrouped'
+    .PARAMETER Search
+        Search
+    .EXAMPLE
+        Get-SophosCentralFirewall -GroupId 'ungrouped'
+        List firewalls that aren't part of a firewall group.
+    .EXAMPLE
+        Get-SophosCentralFirewall -GroupId 'e25ec1f2-04f5-477e-a10d-71f2e039ebaf'
+        List firewalls in the group e25ec1f2-04f5-477e-a10d-71f2e039ebaf
     .LINK
         https://developer.sophos.com/docs/firewall-v1/1/routes/firewalls/get
     #>
     [CmdletBinding()]
     param (
+        [ValidateScript({
+                if ($_ -eq 'ungrouped') {
+                    return $true
+                } elseif ($false -eq [System.Guid]::TryParse($_, $([ref][guid]::Empty))) {
+                    throw 'Not a valid GUID' 
+                } else {
+                    return $true
+                }
+            })]
+        [string]$GroupId,
+        [string]$Search
     )
     Test-SophosCentralConnected
     
-    $uri = [System.Uri]::New($SCRIPT:SophosCentral.RegionEndpoint + '/firewall/v1/firewalls')
+    $uriTemp = [System.Uri]::New($SCRIPT:SophosCentral.RegionEndpoint + '/firewall/v1/firewalls?pageTotal=true')
+    $uri = New-UriWithQuery -Uri $uriTemp -OriginalPsBoundParameters $PsBoundParameters
     Invoke-SophosCentralWebRequest -Uri $uri
 }
