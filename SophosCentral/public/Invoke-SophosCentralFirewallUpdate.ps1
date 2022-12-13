f948844de4bc410688e4a1dfb7e7bb86da1b5f52
Simon Watson <sw@simon-watson.com>
2022-12-13 14:35:33 +0800

    fix link to documentation

--- SophosCentral/public/Invoke-SophosCentralFirewallUpdate.ps1
+++ SophosCentral/public/Invoke-SophosCentralFirewallUpdate.ps1
@@ -15,7 +15,7 @@ function Invoke-SophosCentralFirewallUpdate {
     .EXAMPLE
         Invoke-SophosCentralFirewallUpdate -FirewallID "6d41e78e-0360-4de3-8669-bb7b797ee515" -UpgradeToVersion 'some-version' -UpgradeAt (Get-Date).AddHours(8)
     .LINK
-        https://developer.sophos.com/docs/endpoint-v1/1/routes/endpoints/%7BendpointId%7D/update-checks/post
+        https://developer.sophos.com/docs/firewall-v1/1/routes/firewalls/actions/firmware-upgrade/post
     #>
     [CmdletBinding(SupportsShouldProcess, ConfirmImpact = 'High')]
     param (
